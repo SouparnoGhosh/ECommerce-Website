e35824241333161312cf8b02ac1b9af90c9ab231
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-07-28 07:46:23 +0530

    M

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,20 @@ import { FaBeer } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div className="text-4xl bg-met-red text-yellow-50 px-4">
-      <h3 className="text-3xl">Hey </h3>
+    <div className="text-xl bg-met-red text-yellow-50 px-4 flex justify-between py-4">
+      <div>
+        <h3 className="text-2xl font-bold">Guacamole</h3>
+      </div>
+
+      <div className="flex justify-around gap-6">
+        <div>Collections</div>
+        <div>Sign Up</div>
+        <input
+          type="text"
+          placeholder="Search..."
+          className="rounded-xl py-1 px-2 pl-4"
+        ></input>
+      </div>
     </div>
   );
 };
