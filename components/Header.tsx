1a5329748cc47076081ef5e1279b05b9f1c2d5e0
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-07-30 08:36:45 +0530

    added 1 layer nav bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,36 @@
 import { FaBeer } from "react-icons/fa";
+import Link from "next/link";
+import { menuItems } from "./menuItems";
 
 const Header = () => {
   return (
     <div className="text-xl bg-met-red text-yellow-50 px-4 flex justify-between py-4">
       <div>
-        <h3 className="text-2xl font-bold">Guacamole</h3>
+        <h3 className="text-2xl font-bold">
+          <Link href="/#">
+            <a>Guacamole</a>
+          </Link>
+        </h3>
       </div>
 
-      <div className="flex justify-around gap-6">
-        <div>Collections</div>
-        <div>Sign Up</div>
+      <nav className="flex justify-around gap-6">
+        <ul className="flex justify-around gap-6">
+          {menuItems.map((menu, index) => {
+            return (
+              <li key={index}>
+                <Link href="/#">
+                  <a>{menu.title}</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
         <input
           type="text"
           placeholder="Search..."
           className="rounded-xl py-1 px-2 pl-4"
         ></input>
-      </div>
+      </nav>
     </div>
   );
 };
---
+++ components/menuItems.tsx
@@ -0,0 +1,17 @@
+export const menuItems = [
+  {
+    title: "Home",
+  },
+  {
+    title: "Services",
+  },
+  {
+    title: "About",
+  },
+  {
+    title: "Collections",
+  },
+  {
+    title: "Sign Up",
+  },
+];
